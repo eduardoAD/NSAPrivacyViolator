4578a3237fd01ff90f1d6cc5ff69c140de885d98
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-15 11:00:07 -0500

    Initial Commit

---
+++ NSAPrivacyViolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSAPrivacyViolator
+//
+//  Created by Eduardo Alvarado Díaz on 10/15/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
